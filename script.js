add7e2a16136e145adbea3c09bd25375a62a3226
tulieu-hbt <tulieugiangday.hbt@gmail.com>
2024-11-13 19:12:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,13 @@ const preservationTexts = {
                  Vitamin C: Giúp tăng cường hệ miễn dịch. Vitamin B: Quan trọng cho hệ thần kinh và chức năng trao đổi chất. Kali: Giúp điều hòa huyết áp.\
                  Ẩm thực: Trái bầu thường được sử dụng trong nhiều món ăn, từ canh, xào, đến món hầm. Nó có hương vị nhẹ nhàng, thanh mát, rất phù hợp cho. \
                  các món ăn mùa hè. Dược liệu: Bầu có tính mát, giúp giải nhiệt, thanh lọc cơ thể. Nước ép từ bầu cũng được cho là có tác dụng làm mát gan. \ 
-                 và hỗ trợ tiêu hóa. Vỏ trái bầu sau khi khô có thể được sử dụng để làm đồ dùng gia đình như bình đựng nước, hộp đựng, hoặc trang trí..."
+                 và hỗ trợ tiêu hóa. Vỏ trái bầu sau khi khô có thể được sử dụng để làm đồ dùng gia đình như bình đựng nước, hộp đựng, hoặc trang trí...",
+    "trái mướp: "Trái mướp, còn được gọi là mướp hương hay mướp gối. Tên khoa học: Luffa cylindrica (cho mướp hương) và Luffa acutangula (cho mướp gối \
+                Trái mướp chứa nhiều dưỡng chất cần thiết như: Vitamin C: Giúp tăng cường hệ miễn dịch. Vitamin B: Quan trọng cho hệ thần kinh và chức năng \
+                trao đổi chất. Chất xơ: Giúp cải thiện tiêu hóa và duy trì sức khỏe đường ruột. Trái mướp thường được sử dụng trong nhiều món ăn, từ canh mướp, \
+                xào mướp, đến nấu lẩu. Mướp có vị ngọt thanh, mát, rất thích hợp cho các món ăn mùa hè. Dược liệu: Mướp có tính mát, giúp giải nhiệt, thanh lọc cơ thể. \
+                Nước ép từ mướp cũng được cho là có tác dụng làm mát gan và hỗ trợ tiêu hóa. Vỏ trái mướp sau khi khô có thể được sử dụng để làm xơ mướp, một loại \
+                vật liệu thiên nhiên dùng để cọ rửa hoặc tắm."
  };
 
 // Khởi tạo camera
